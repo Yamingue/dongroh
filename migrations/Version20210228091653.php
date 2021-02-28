14c3cc71c1432e5e20df78f38b1e7b165fe35693
Yamingue Kingatal Franck <yamking01@gmail.com>
2021-02-28 11:06:07 +0100

    Gestion de la connexion admin

--- migrations/Version20210228091653.php
+++ migrations/Version20210228091653.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210228090455 extends AbstractMigration
+final class Version20210228091653 extends AbstractMigration
 {
     public function getDescription() : string
     {
