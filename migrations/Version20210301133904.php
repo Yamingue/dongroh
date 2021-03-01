dfee18227cce8169a18c3f5d62e8c4b3952fff7f
Yamingue Kingatal Franck <yamking01@gmail.com>
2021-03-01 14:39:54 +0100

    migration

--- migrations/Version20210301133904.php
+++ migrations/Version20210301133904.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210301131306 extends AbstractMigration
+final class Version20210301133904 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,6 +20,7 @@ final class Version20210301131306 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE admin (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_880E0D76E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE arcticle_commande (id INT AUTO_INCREMENT NOT NULL, qte INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE arcticle_commande_article (arcticle_commande_id INT NOT NULL, article_id INT NOT NULL, INDEX IDX_BA273CBCF6328AD0 (arcticle_commande_id), INDEX IDX_BA273CBC7294869C (article_id), PRIMARY KEY(arcticle_commande_id, article_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE article (id INT AUTO_INCREMENT NOT NULL, titre VARCHAR(255) NOT NULL, prix INT NOT NULL, photo VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
@@ -82,6 +83,7 @@ final class Version20210301131306 extends AbstractMigration
         $this->addSql('ALTER TABLE note_article DROP FOREIGN KEY FK_839CCAFC60BB6FE6');
         $this->addSql('ALTER TABLE panier DROP FOREIGN KEY FK_24CC0DF2A76ED395');
         $this->addSql('ALTER TABLE reset_password_request DROP FOREIGN KEY FK_7CE748AA76ED395');
+        $this->addSql('DROP TABLE admin');
         $this->addSql('DROP TABLE arcticle_commande');
         $this->addSql('DROP TABLE arcticle_commande_article');
         $this->addSql('DROP TABLE article');
