5eb26b3724427f1276db32e7b8dfe2ca0be31f66
Yamingue Kingatal Franck <yamking01@gmail.com>
2021-03-03 20:08:30 +0100

    migration de la bd

--- migrations/Version20210303190809.php
+++ migrations/Version20210303190809.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210301133904 extends AbstractMigration
+final class Version20210303190809 extends AbstractMigration
 {
     public function getDescription() : string
     {
