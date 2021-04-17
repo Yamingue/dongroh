b7f56f6122fb86580a16bf66e4dcb1b54bcaf9ec
Yamingue Kingatal Franck <yamking01@gmail.com>
2021-04-17 23:49:04 +0200

    retrait de turbo

--- assets/app.js
+++ assets/app.js
@@ -11,16 +11,13 @@ import './styles/app.scss';
 // this loads jquery, but does *not* set a global $ or jQuery variable
 const $ = require('jquery');
 global.$ = global.jQuery = $;
-require ('mdbootstrap/js/popper')
-require('chart.js')
-import 'mdbootstrap/js/mdb'
+require('bootstrap')
+require ('./mdb/js/mdb.ecommerce.min.js');
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 // create global $ and jQuery variables
 
 AOS.init();
-var Turbolinks = require("turbolinks")
-Turbolinks.start()
 
 // start the Stimulus application
 import './bootstrap';
