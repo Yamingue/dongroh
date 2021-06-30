390768bcccca63cf6e6183a1d990249c75fe05eb
Yamingue <yamking01@gmail.com>
2021-06-30 15:46:28 +0200

    bundle avec encore

---
+++ assets/admin/admin.js
@@ -0,0 +1,13 @@
+{/* <script src="/js/jquery-3.5.0.min.js"></script>
+<script src="/js/bootstrap.bundle.min.js"></script>
+
+<!-- ChartJS files-->
+<script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+
+<!-- Custom JS -->
+<script src="/js/scriptc619.js"></script> */}
+
+import './admin.scss'
+import './js/jquery-3.5.0.min.js'
+import './js/bootstrap.bundle.min.js'
+import './js/scriptc619.js'
